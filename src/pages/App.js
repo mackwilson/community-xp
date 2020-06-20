90502b2111628d2906fb053b815fbbfe44ec2c17
mackwilson <ms2wilso@uwaterloo.ca>
2020-06-20 19:10:15 -0400

    Reorganize file structure

---
+++ src/pages/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../assets/cuxp-logo.png';
+import '../css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Welcome to CommUnity XP! We are currently under construction. Come back soon!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
