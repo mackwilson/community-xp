38d2f692cf72960c18c060ad9f26bc54717131e9
mackwilson <ms2wilso@uwaterloo.ca>
2020-06-20 19:20:41 -0400

    Add a comp

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import logo from '../assets/cuxp-logo.png';
 import '../css/App.css';
+import Header from '../comps/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to CommUnity XP! We are currently under construction. Come back soon!
-        </p>
-      </header>
+      <Header />
     </div>
   );
 }
