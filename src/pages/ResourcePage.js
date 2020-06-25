b997af2e6a8e19d842145b6bf0866e886a10e9f0
mackwilson <ms2wilso@uwaterloo.ca>
2020-06-25 13:20:18 -0400

    Add tabs to resources page

--- src/pages/ResourcePage.js
+++ src/pages/ResourcePage.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PublicNavbar from '../comps/PublicNavbar.js';
 import BigHeader from '../comps/BigHeader.js';
 import Footer from '../comps/Footer.js';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import '../css/App.css';
 
 export default class ResourcePage extends React.Component {
@@ -12,6 +14,19 @@ export default class ResourcePage extends React.Component {
         <PublicNavbar />
         <div className="content">
           <BigHeader title="Resources" txt={text}/>
+          <div className="left-text">
+            <Tabs defaultActiveKey="category" id="uncontrolled-tab-example">
+              <Tab eventKey="category" title="By Category">
+                <p className="left-text">Resources by category </p>
+              </Tab>
+              <Tab eventKey="near" title="Near Me">
+                <p className="left-text">Resources by location</p>
+              </Tab>
+              <Tab eventKey="quiz" title="Resource Matcher">
+                <p className="left-text">Take the Resource Matcher quiz! Tell us about yourself and we will suggest a collection of resources.</p>
+              </Tab>
+            </Tabs>
+          </div>
           <Footer />
         </div>
       </div>
