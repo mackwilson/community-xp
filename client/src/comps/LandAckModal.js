6a54fdebefed960d504f4f76fbe6ee67eb7258d6
mackwilson <ms2wilso@uwaterloo.ca>
2021-04-25 21:17:46 -0400

    Introduce state-controlled land ack modal
    First pass at implementing a modal on the first page that starts open, stays open for 5s, and contains land acknowledgement information. TBD what this information will be. This modal is controlled by state, although the state is not persisted.
    
    This PR accomplishes the set up of the modal - currently designed the same way as the team modals - and making it non-closeable for 5 seconds after page load. Currently the modal shows every time we navigte to the home page, despite being controlled by state.
    
    Importantly, this starts a state slice for th broader user object.

---
+++ client/src/comps/LandAckModal.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+
+import { useSelector, useDispatch } from 'react-redux'
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+import {
+  getShowLandAck,
+  viewLandAck
+} from '../store/userSlice.js';
+
+import text from '../assets/text/pageText.json';
+
+
+export default function LandAckModal(props) {
+  const dispatch = useDispatch();
+  const handleClose = () => dispatch(viewLandAck());
+
+  const [closeable, setCloseable] = useState(false);
+  const show = useSelector(getShowLandAck);    
+  
+  const headerWithClose = <Modal.Header closeButton />;
+  const headerNoClose = <Modal.Header />;
+
+  
+  // don't show the close button for 5s, encourage user to actually read
+  useEffect(() => {
+    setTimeout(() => {
+      setCloseable(true)
+    }, 5000)
+   }, [])
+  
+  return (
+    <div>
+      <Modal 
+        show={show} 
+        onHide={handleClose} 
+        centered 
+        backdrop="static" // prevent user from clicking outside to close modal
+        keyboard={ false } // prevent user from using ESC key to close modal
+      >
+      
+        { closeable ? headerWithClose : headerNoClose }
+        
+        <Modal.Body className="greyFill whiteText centre-text">
+        
+          <Modal.Title centered className="pinkText centre-text spaceBottom">
+            <h3>{text.home.land.title}</h3>
+          </Modal.Title>
+          
+          <p>[icons: fist, rainbow flag]</p>
+          <p className="width-80 spaceTop spaceBottom">{text.home.land.text}</p>
+          <Button href={text.home.land.button.link} target="_blank" className="spaceTop spaceBottom darkText blueFill pinkShadow btn">
+            {text.home.land.button.text}
+          </Button>   
+                   
+        </Modal.Body>
+        
+      </Modal>
+    </div>
+  );
+}
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import resourcesReducer from './resourcesSlice';
+import userReducer from './userSlice';
 
 export default configureStore({
   reducer: {
-    resources: resourcesReducer
+    resources: resourcesReducer,
+    user: userReducer
   }
 });
