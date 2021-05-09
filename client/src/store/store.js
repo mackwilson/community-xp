a1ef2135395c88ef401417b5d3dabe10bc4a5389
mackwilson <ms2wilso@uwaterloo.ca>
2021-05-09 17:49:44 -0400

    Introduce redux-persist persisted store
    Install and implement redux-persist to persist state to redux store
    across page transitions and reloads. Storage is currently set to session
    so that state will be reloaded on tab or window close, but not on
    refresh or navigation. This is to better control when to show the land
    ack modal and will also be useful for user authentication.

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,11 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { persistStore, persistReducer } from 'redux-persist'
+// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import storageSession from 'redux-persist/lib/storage/session'
+
 import resourcesReducer from './resourcesSlice';
 import userReducer from './userSlice';
 
-export default configureStore({
-  reducer: {
-    resources: resourcesReducer,
-    user: userReducer
-  }
-});
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+}
+
+export default () => {
+  let store = configureStore({
+    reducer: {
+      resources: persistReducer(persistConfig, resourcesReducer),
+      user: persistReducer(persistConfig, userReducer)
+    }
+  });
+  let persistor = persistStore(store);
+  return { store, persistor }
+}
